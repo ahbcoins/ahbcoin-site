07ea5abd3ae7280cb712727bb8862caa20e52897
ahbcoins <cctvahb@gmail.com>
2025-12-08 02:11:16 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-
-function simulateBurn() {
-  let current = document.getElementById("burnAmount").innerText.split(" ")[0];
-  let newAmount = Number(current) + 1000;
-  document.getElementById("burnAmount").innerText = newAmount + " AHB";
-}
