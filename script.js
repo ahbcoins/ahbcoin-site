2cc81ba2e841b2e26d8bf9bdba1093da59daa3aa
ahbcoins <cctvahb@gmail.com>
2025-12-08 02:20:00 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function toggleTheme(){
+  document.body.classList.toggle("dark");
+}
+function buyNow(){
+  window.open("https://app.uniswap.org", "_blank");
+}
+function openChart(){
+  window.location.href = "#chart";
+}
+function goHome(){ window.scrollTo(0,0); }
+function swap(){ alert("Swap feature coming soon"); }
+function info(){ alert("AHB Info section"); }
